55df18f009f23fbce8eba0cd9bf91b8242b3618f
IlyaValikov <ilya.valikov1@yandex.ru>
2024-02-09 21:19:58 +0300

    upd

--- internal/app/app.go
+++ internal/app/app.go
@@ -12,35 +12,34 @@ import (
 
 // Application представляет основное приложение.
 type Application struct {
-    Config    *config.Config
-    Repository *repository.Repository
+	Config     *config.Config
+	Repository *repository.Repository
 	UseCase    *usecase.UseCase
 	Handler    *Request.Handler
 }
 
-// New создает новый объект Application и настраивает его.
+// New создает новый объект Application.
 func New(ctx context.Context) (*Application, error) {
-    // Инициализируйте конфигурацию
-    cfg, err := config.NewConfig(ctx)
-    if err != nil {
-        return nil, err
-    }
+	// Инициализируйте конфигурацию
+	cfg, err := config.NewConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
 
-    // Инициализируйте подключение к базе данных (DB)
-    repo, err := repository.New(dsn.FromEnv())
-    if err != nil {
-        return nil, err
-    }
-    uc := usecase.NewUseCase(repo)
-    h := Request.NewHandler(uc)
-    // Инициализируйте и настройте объект Application
-    app := &Application{
-        Config: cfg,
-        Repository: repo,
-        UseCase: uc,
-        Handler: h,
-    }
+	// Инициализируйте подключение к базе данных (DB)
+	repo, err := repository.New(dsn.FromEnv())
+	if err != nil {
+		return nil, err
+	}
+	uc := usecase.NewUseCase(repo)
+	h := Request.NewHandler(uc)
+	// Инициализируйте и настройте объект Application
+	app := &Application{
+		Config:     cfg,
+		Repository: repo,
+		UseCase:    uc,
+		Handler:    h,
+	}
 
-    return app, nil
+	return app, nil
 }
-
